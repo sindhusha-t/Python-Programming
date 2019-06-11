122d819061dcb1f43efb4c3a2011b6b61971991b
sindhusha-t <sindhusha55@gmail.com>
2019-06-11 23:41:10 +0530

    ICP-

--- ICP-3/employee.py
+++ ICP-3/employee.py
@@ -9,8 +9,8 @@ class Employee:
         self.salary = salary
         self.department = department
         
-        self.__class__.number_of_employees += 1
-        self.__class__.total_salary += self.salary
+        Employee.number_of_employees += 1
+        Employee.total_salary += self.salary
 
     def getAverageSalary():
         avg = Employee.total_salary / Employee.number_of_employees
@@ -21,21 +21,25 @@ class FulltimeEmployee(Employee):
     def __init__(self, name, age, salary, department):
         Employee.__init__(self, name, age, salary, department)
 
-if __name__ == "__main__":
+    def createObjects():
+        print("--------Employee Database---------\n")
     
-    print("--------Employee Database---------\n")
-    
-while True:
-    bool = input("\n Do you want to enter employee details (yes/no): ")
-    if bool == "yes":
-        name = input("Enter Employee name: ")
-        age = int(input("Enter Employee age: "))
-        salary = int(input("Enter Employee salary: "))
-        department = input("Enter Employee Department: ")
+        while True:
+            bool = input("\n Do you want to enter employee details (yes/no): ")
+            
+            if bool == "yes":
+                name = input("Enter Employee name: ")
+                age = int(input("Enter Employee age: "))
+                salary = int(input("Enter Employee salary: "))
+                department = input("Enter Employee Department: ")
         
-        Employee(name, age, salary, department)
+                FulltimeEmployee(name, age, salary, department)
     
-    else:
-        print("Total Number of Employees: " + str(Employee.number_of_employees))
-        print("Final Average Salary of Employees: " + str(Employee.getAverageSalary()))
-        break
+            else:
+                print("Total Number of Employees: " + str(Employee.number_of_employees))
+                print("Final Average Salary of Employees: " + str(Employee.getAverageSalary()))
+                break
+        
+if __name__ == "__main__":
+    FulltimeEmployee.createObjects()
+    
